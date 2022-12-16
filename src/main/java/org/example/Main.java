e724350396a8cbd7a72ed022853bd6fe8df9e7a1
line-t-c <line_t_c@hotmail.com>
2022-12-16 15:02:00 +0100

    Changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("world!");
+        System.out.println("world!");
+    }
+}
